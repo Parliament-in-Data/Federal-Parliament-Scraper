e24786ebd19a4b68ba42d8eb17c4b5057a45e278
nielsdos <7771979+nielsdos@users.noreply.github.com>
2021-05-11 20:32:01 +0200

    Fix demo

--- demo.py
+++ demo.py
@@ -1,21 +1,21 @@
 #%%
 from parliament_session import ParliamentarySession
-session = ParliamentarySession(55)
+from data_store import CompoundDataStore
+from models.enums import TopicType
+#%%
+session = ParliamentarySession(55, CompoundDataStore([]))
 # Get an object containing all known members during the session
 session.get_members()
 # Get all plenary meeting
 meetings = session.get_plenary_meetings()
 # %%
 # For a single meeting a lot of information is available
-topics = meetings[0].get_meeting_topics() # <- This is a dict mapping the agenda item number onto an object
+topics = meetings[0].topics[TopicType.NAME_VOTE]
 for idx in reversed(list(topics.keys())):
-    print("%d. %s" % (idx, topics[idx].get_title()[0])) # <- Textual objects (titles and section names) are stored as tuples 
-    if topics[idx].get_votes():                         #    containing NL on position 0 and FR on position 1.
-        vote = topics[idx].get_votes()[0]
+    print(f"{idx}. {topics[idx].title.NL}")
+    if topics[idx].votes:
+        vote = topics[idx].votes[0]
         print(vote)
         print("Ja: %s" % ([str(voter) for voter in vote.yes_voters]))
         print("Nee: %s" % ([str(voter) for voter in vote.no_voters]))
         print("Onthouding: %s" % ([str(voter) for voter in vote.abstention_voters]))
-#%%
-for meeting in meetings:
-    meeting.get_meeting_topics()
